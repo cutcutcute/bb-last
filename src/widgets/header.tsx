09eddab5c867d14365f4c9826b96d8ad97770147
cutcutcute <125074247+cutcutcute@users.noreply.github.com>
2024-03-12 01:45:22 +0300

    добавил сайдбар к хэдеру и загрузку

--- src/widgets/header.tsx
+++ src/widgets/header.tsx
@@ -1,12 +1,43 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, {useRef} from "react";
+import { motion, useCycle } from "framer-motion";
 import { NavigationItem } from "../shared/navigationItem";
+import { useDimensions } from "../features/hooks/useDimensions";
+import { Navigation } from "./Navigation";
+import { MenuToggle } from "../shared/buttons/MenuToggle";
+import { useParallax } from "react-scroll-parallax";
+
+const sidebar = {
+    open: (height = 800) => ({
+      clipPath: `circle(${height * 1 + 100}px at 20px 20px)`,
+      transition: {
+        type: "spring",
+        stiffness: 20,
+        restDelta: 2
+      }
+    }),
+    closed: {
+      clipPath: "circle(20px at 40px 40px)",
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 400,
+        damping: 40
+      }
+    }
+  };
 
 
 export const Header = (): React.JSX.Element => {
-    return <motion.header initial={{opacity:0, y:-90}} animate={{opacity:1, y:0}} transition={{duration:.8}} className="page-header px-2">
+
+    const [isOpen, toggleOpen] = useCycle(false, true);
+    const containerRef = useRef(null);
+    const { height } = useDimensions(containerRef);
+   
+
+    return <> 
+    <motion.header  initial={{opacity:0, y:-90}} animate={{opacity:1, y:0}} transition={{duration:.8}} className="page-header px-2">
         <nav className="navigation w-100" >
-            <div className="container w-100">
+            <div className="container navigation-container w-100">
                 <div className="row align-items-end">
                     <div className="col"><NavigationItem textValue="Портфолио" textSizeLevel={1}/></div>
                     <div className="col"><NavigationItem textValue="Услуги" textSizeLevel={2}/></div>
@@ -17,4 +48,22 @@ export const Header = (): React.JSX.Element => {
             </div>
         </nav>
     </motion.header>
-} 
+    <motion.div className={`${isOpen?"open":"close"} blur-mobile`} initial={false}
+            animate={isOpen ? "open" : "closed"}
+            custom={height}
+            ref={containerRef}
+           ></motion.div>
+            
+
+    <motion.nav className="header-mobile-nav"
+            initial={false}
+            animate={isOpen ? "open" : "closed"}
+            custom={height}
+            ref={containerRef}
+        >
+        <motion.div className="background-sidebar" variants={sidebar} />
+            <Navigation />
+            <MenuToggle toggle={() => toggleOpen()} />
+        </motion.nav>
+         </>
+    } 
--- src/widgets/loading/mainLoading.tsx
+++ src/widgets/loading/mainLoading.tsx
@@ -3,9 +3,10 @@ import "../../app/styles/styles.scss";
 import { LogoMax } from "../../shared/logo/logoMax";
 
 export const MainLoading = ():React.JSX.Element =>{
-    return <section className="loading-window">
-
-            <LogoMax/>
-
-    </section>
+    return <section className="loading-wrapper">
+    <div className="loadingio-spinner-spinner-fuzb31wykg"><div className="ldio-m3nzsox4fo">
+    <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+    </div></div> </section>
+   
+    
 }
